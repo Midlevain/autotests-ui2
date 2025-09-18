aa5f7c42b1908871f2e43da8720d4449437f5c8f
Midlevain <midlevainwork@mail.ru>
2025-09-18 21:44:27 +0300

    Знакомство с pagecomponent.Практика работы с page component

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,3 +1,5 @@
+from typing import Pattern
+
 from playwright.sync_api import Page, expect
 
 from  components.base_component import BaseComponent
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,27 +1,18 @@
-from playwright.sync_api import expect
+from playwright.sync_api import expect,Page
 import pytest
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
+def test_empty_courses_list(courses_list_page: CoursesListPage):
 
-    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-    courses_list_toolbar = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_list_toolbar).to_be_visible()
-
-    courses_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(courses_icon).to_be_visible()
-
-    courses_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(courses_view_title).to_be_visible()
-    expect(courses_view_title).to_have_text('There is no results')
-
-    courses_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(courses_view_description).to_be_visible()
-    expect(courses_view_description).to_have_text('Results from the load test pipeline will be displayed here')
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
     #python -m pytest -s -v -k "test_empty_courses_list"
 
