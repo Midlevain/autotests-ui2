6a23279dd40a59da42664f73655c952b9585ccb8
Midlevain <midlevainwork@mail.ru>
2025-11-04 21:28:21 +0300

    Наращивание тестовой базы и рефакторинг

---
+++ tests/authentication/test_registration.py
@@ -0,0 +1,24 @@
+import pytest
+
+from fixtures.pages import registration_form_component
+from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
+from components.authentication.registration_form_component import RegistrationFormComponent
+from components.dashboard.dashboard_toolbar_view_component import DashboardToolbarViewComponent
+
+
+@pytest.mark.regression
+@pytest.mark.registration
+class TestRegistration:
+        def test_successful_registration(
+            self,
+            registration_page: RegistrationPage,
+            dashboard_page: DashboardPage,
+            registration_form_component: RegistrationFormComponent,
+            dashboard_toolbar_view_component: DashboardToolbarViewComponent
+        ):
+                registration_page.visit(
+                        'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+                registration_form_component.fill(email="user.name@gmail.com", username="username", password="password")
+                registration_page.click_registration_button()
+                dashboard_page.dashboard_title.check_visible()
